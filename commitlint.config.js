2aedd8e28ffe2a030535c40b45c85c79c959cbc4
Joel Mathew Koshy <joelmathewkoshy@gmail.com>
2023-06-09 13:44:01 -0400

    feat(global): conventional commits with commitlint for best practices while committing changes

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ["@commitlint/config-conventional"],
+  /*
+   * Any rules defined here will override rules from @commitlint/config-conventional
+   */
+  rules: {
+    "scope-enum": [2, "always", ["global", "api"]],
+  },
+};
