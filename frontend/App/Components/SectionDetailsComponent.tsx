43c5cba757a80cd5d7cba6a43877d6fb214ef2ce
Luqmaan Shaik <luqmaanshaik@gmail.com>
2023-06-22 13:38:34 -0400

    MYG-69/ UI for section quiz

--- frontend/App/Components/SectionDetailsComponent.tsx
+++ frontend/App/Components/SectionDetailsComponent.tsx
@@ -1,13 +1,65 @@
+import { CommonActions, useNavigation } from "@react-navigation/core";
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const SectionDetailsComponent = ({ route }) => {
-  const { sectionTitle, sectionDescription } = route.params;
+  const { sectionTitle, sectionDescription, sectionTopics, navigation } =
+    route.params;
+
+  const handleButtonPress = (topic) => {
+    // Request body for the API call
+    const requestBody = {
+      section: topic,
+    };
+
+    // API call to fetch topics
+    fetch("https://api.example.com/topics", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestBody),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Process the fetched topics
+
+        // Navigate to a different screen
+        navigation.navigate("TopicDetails", { topics: data });
+      })
+      .catch((error) => {
+        console.error("Error fetching topics:", error);
+      });
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.sectionTitle}>{sectionTitle}</Text>
       <Text style={styles.sectionDescription}>{sectionDescription}</Text>
+      <View style={{ marginTop: 20 }}>
+        {sectionTopics.map((value, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              style={styles.topic}
+              onPress={() =>
+                //handleButtonPress(value)
+                navigation.navigate("TopicDetails", {
+                  topicId: 1,
+                  topicDescription: "Loren Ipsum",
+                  topicSignificance: "high",
+                  topicVideo: "www.URL.com",
+                  navigation: navigation
+                })
+              }
+            >
+              <View style={styles.textContainer}>
+                <Text style={styles.sectionTitle}>{value}</Text>
+              </View>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
     </View>
   );
 };
@@ -15,9 +67,9 @@ const SectionDetailsComponent = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
-    padding: 20,
+    paddingTop: 40,
   },
   sectionTitle: {
     fontSize: 18,
@@ -27,6 +79,26 @@ const styles = StyleSheet.create({
   sectionDescription: {
     fontSize: 16,
     color: "#333",
+  },
+  topic: {
+    backgroundColor: "#fff",
+    borderRadius: 8,
+    marginBottom: 16,
+    padding: 24,
+    width: "60%",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    flexDirection: "row",
+    alignItems: "flex-start",
+  },
+  textContainer: {
+    flex: 1,
   },
 });
 
---
+++ frontend/App/Components/TopicDetails.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import Quiz from "./Quiz";
+
+function TopicDetails({ route }) {
+  const {
+    topicId,
+    topicDescription,
+    topicSignificance,
+    topicVideo,
+    navigation,
+  } = route.params;
+
+  const handleStartQuiz = () => {
+    navigation.navigate("Quiz",{navigation:navigation});
+  };
+  return (
+    <View style={{ margin: 18 }}>
+      <Text style={styles.description}>{topicDescription}</Text>
+      <Text style={styles.significance}>{topicSignificance}</Text>
+      <View style={styles.videoContainer}>
+        {/* video component goes here */}
+      </View>
+      <TouchableOpacity
+        style={styles.startQuizButton}
+        onPress={handleStartQuiz}
+      >
+        <Text style={styles.startQuizButtonText}>Start Quiz</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  description: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  significance: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  videoContainer: {
+    width: "100%",
+    aspectRatio: 16 / 9, // Adjust the aspect ratio as per your video's dimensions
+    backgroundColor: "black",
+  },
+  startQuizButton: {
+    backgroundColor: "#0782F9",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  startQuizButtonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
+export default TopicDetails;
