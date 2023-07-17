83e405d699f7fab474b221920f1a5e042065fe30
Kajal Singh <kajalsingh@Kajals-MacBook-Air.local>
2023-07-17 16:51:19 -0400

    backend for premium subscription

--- api/src/controllers/userController.ts
+++ api/src/controllers/userController.ts
@@ -144,7 +144,34 @@ const changePassword = async (req: Request, res: Response) => {
   });
 };
 
+const upgradeToPremium = async (req: Request, res: Response) => {
+  const userEmail = req.body.userEmail; 
+  try {
+    // Find the user in the database based on their email
+    const user = await User.findOne({ where: { email: userEmail } });
+    if (!user) {
+      return res.status(500).json({
+        status: false,
+        payload: {
+          message: "User does not exist",
+        },
+      });
+    }
+    user.isPremium = true;
+    await user.save();
 
+    return res.status(201).json({
+      status: true,
+      payload: {
+        message: "User upgraded to premium successfully",
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      message: 'Failed to upgrade user to premium.',
+    });
+  }
+};
 
-
-export { registerUser, users, forgotPassword, changePassword, verifyOtp };
+export { registerUser, upgradeToPremium, users, forgotPassword, changePassword, verifyOtp };
