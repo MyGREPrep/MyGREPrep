04e721a3bc3170d4950754912f6c76d9a06a3284
Joel Mathew Koshy <joelmathewkoshy@gmail.com>
2023-07-18 17:15:41 -0400

    testing running the frontend on web browser

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,8 +1,11 @@
 /**
  * @format
  */
-import 'react-native-gesture-handler';
-import {AppRegistry} from 'react-native';
-import App from './App';
+import "react-native-gesture-handler";
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { registerRootComponent } from "expo";
 
 AppRegistry.registerComponent("MyGREPrep", () => App);
+
+registerRootComponent(App);
